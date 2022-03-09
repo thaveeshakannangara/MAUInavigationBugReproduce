bfc218c99a1afb95f55827c964b3068055d78530
Thaveesha Kannangara <thaveesheka@gmail.com>
2022-03-09 19:45:36 +0530

    first commit

---
+++ App6/IntermediatePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App6
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class IntermediatePage : ContentPage
+    {
+        public IntermediatePage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnNavigate_Clicked(object sender, EventArgs e)
+        {
+            Application.Current.MainPage = new NavigationPage(new MainMenu());
+        }
+    }
+}
